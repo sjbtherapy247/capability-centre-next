10704031f3cef618bbda099850cf72a01df30f23
sjb247-bakes <sjb247@gmail.com>
2026-05-31 01:33:13 +1000

    fix: Sanity Studio missing metadata/viewport exports - restores publish button

--- app/cc-admin/[[...tool]]/page.tsx
+++ app/cc-admin/[[...tool]]/page.tsx
@@ -1,11 +1,10 @@
 /**
  * Sanity Studio route mounted at /cc-admin (basePath set in sanity.config.ts).
- * Renders only on the client; uses the root sanity.config.ts.
  */
-'use client'
-
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
+
+export { metadata, viewport } from 'next-sanity/studio'
 
 export const dynamic = 'force-static'
 
